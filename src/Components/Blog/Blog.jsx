3538260c3ad9d0cd9707be974b1cd125ddb3c4d7
nomayen <nomayen.ohin@gmail.com>
2023-11-01 09:50:23 +0600

    ui design complite

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,30 +1,32 @@
 import PropTypes from 'prop-types'
 import React from 'react';
+import {  FaBookmark } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
     const {title, cover,reading_time,author_img,posted_date,author, hashtags } = blog;
     console.log(blog);
 
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
+        <div className='mb-20'>
+            <img className='w-full mb-8 rounded-xl' src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between'>
                 <div className='flex gap-6'>
                     <img className='w-14' src={author_img} alt="" />
                     <div>
-                        <h3 className='text-2xl'>{author}</h3>
+                        <h3 className='text-2xl mb-2'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                     
                 </div>
-                <div>
+                <div className='flex gap-4'>
                     <span>{reading_time} min read</span>
+                    <span className='mt-1'><FaBookmark></FaBookmark></span>
                 </div>
             </div>
-            <h2 className='text-4xl'>{title}</h2>
+            <h2 className='text-4xl mt-4 mb-4 leading-relaxed tracking-widest'>{title}</h2>
             <p>
                 {
-                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                    hashtags.map((hash, idx) => <span key={idx}><a href=""># {hash} .  </a></span>)
                 }
             </p>
         </div>
